a95b0c42f906a6a8a5da98d93b96ccf1842495d4
Do Nascimento Antoine <antoine.donascimento@student-cs.fr>
2020-11-13 16:22:45 +0100

    convert_direction

--- Antoine.py
+++ Antoine.py
@@ -4,3 +4,14 @@ def snake_avance(snake,direction):
     retourne le serpent avancé de 1
     """
     snake=[snake[0]+direction]+snake[1:]
+
+def convert_direction(dir):
+    if dir='d':
+        return (0,1)
+    elif dir='g':
+        return (0,-1)
+    elif dir='h':
+        return (1,0)
+    elif dir='d':
+        return (-1,0)
+        
