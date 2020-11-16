d37fe98b272f16c5d1572d1115e17888b3282ba5
Yse de Reydet de Vulpillieres <yse.devul@student-cs.fr>
2020-11-16 22:55:29 +0100

    hello

--- Main.py
+++ Main.py
@@ -31,31 +31,31 @@ clock = pygame.time.Clock()
 # fonction
 pomme_coupe = [0, 0, False]
 game_over = False
+game_close = False
 border = True
 collision = True
 
 
-def pomme_coupe(score, pomme_coupe):
+def pomme_coupe2(score, pomme_coupe):
     if not pomme_coupe[2] and score > 5:
-        s = random.randint(0, 50)
+        s = random.randint(0, 101)
         if s == 0:
             pomme_coupe[0] = random.randint(0, (L-20)/20)*20
             pomme_coupe[1] = random.randint(0, (H-20)/20)*20
             pomme_coupe[2] = True
-            pygame.draw.rect(
-                dis, vert, [pomme_coupe[0], pomme_coupe[1], 20, 20])
 
 
-def coll_pomme_coupe(l, pomme_coupe):
+def coll_pomme_coupe(l, score, pomme_coupe):
+    l1 = l
     if pomme_coupe[2]:
         if l[0][0] == pomme_coupe[0] and l[0][1] == pomme_coupe[1]:
             score += 1
             taille = len(l)//2
-            l = l[0:taille]
-            print(l)
+            l1 = l[0:taille]
             pygame.draw.rect(
                 dis, black, [pomme_coupe[0], pomme_coupe[1], 10, 10])
             pomme_coupe[2] = False
+    return l1, score
 
 
 def detection_collision_bordure(l, border, game_over):
@@ -77,82 +77,142 @@ def detection_auto_collision(l, collision, game_over):
 
 # fin fonction
 
+def game_loop(border=False):
+    pomme_coupe = [0, 0, False]
+    game_over = False
+    game_close = False
+    collision = True
+    vert = (0, 255, 0)
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    red = (255, 0, 0)
+    violet = (127, 0, 255)
+    green = (0, 255, 65)
+    L = 800
+    H = 600
+    dx = 0
+    dy = 0
+    l = [[300, 300], [280, 300], [260, 300]]
+    pomme = [100, 100]
+    score = 0
+    n = 3
+    while not game_over:
 
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            game_over = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                dx = -20
-                dy = 0
-            elif event.key == pygame.K_RIGHT:
-                dx = 20
-                dy = 0
-            elif event.key == pygame.K_UP:
-                dx = 0
-                dy = -20
-            elif event.key == pygame.K_DOWN:
-                dx = 0
-                dy = 20
+        while game_close == True:
+            police = pygame.font.SysFont('times new roman', 90)
+            game_over_surface = police.render(
+                'Game over', True, (255, 0, 0))  # decription
+            # on récupère les coordonées du rectancle game_over_surface
+            game_over_rect = game_over_surface.get_rect()
+            game_over_rect.midtop = (800/2, 600/4)  # positionnement
+            dis.fill(black)
+            dis.blit(game_over_surface, game_over_rect)  # affiche
 
-    # on avance
-    queue = copy(l[n-1])
-    for k in range(0, n-1):
-        l[n-1-k] = copy(l[n-2-k])
-    l[0][0] += dx
-    l[0][1] += dy
-    dis.fill(black)
+            police_message = pygame.font.SysFont('times', 20)
+            message_surface = police_message.render(
+                'Press Q to quit game and C to restart', True, (255, 0, 0))
+            message_rect = message_surface.get_rect()
+            message_rect.midtop = (800/2, 600/1.5)
+            dis.blit(message_surface, message_rect)
 
-    # detection mur ou soit même
-    #detection_collision_bordure(l, border, game_over)
-    #detection_auto_collision(l, collision, game_over)
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                        pygame.quit()
+                        quit()
+                    if event.key == pygame.K_c:
+                        game_loop()
 
-    # lorsqu'on touche le bord
-    if border and (l[0][0] < 20 or l[0][0] > L-20 or l[0][1] < 20 or l[0][1] > H-20):
-        game_over = True
-    # si bord désactivé on passe de l'autre coté
-    if not border and (l[0][0] < 10 or l[0][0] > L-10 or l[0][1] < 10 or l[0][1] > H-10):
-        l[0][0] = l[0][0] % L
-        l[0][1] = l[0][1] % H
+            pygame.display.flip()
+            time.sleep(1)
 
-    # lorsqu'on se touche
-    for k in range(1, len(l)):
-        if n > 3:
-            if collision and l[0][0] == l[k][0] and l[0][1] == l[k][1]:
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    exit = False
+                    while not(exit):
+                        for event2 in pygame.event.get():
+                            if event2.type == pygame.KEYDOWN:
+                                if event2.key == pygame.K_p:
+                                    exit = True
 
-    # lorsqu'on touche la pomme
-    if pomme == l[0]:
-        score += 1
-        l.append([queue[0], queue[1]])
+                if event.key == pygame.K_LEFT:
+                    dx = -20
+                    dy = 0
+                if event.key == pygame.K_RIGHT:
+                    dx = 20
+                    dy = 0
+                if event.key == pygame.K_UP:
+                    dx = 0
+                    dy = -20
+                if event.key == pygame.K_DOWN:
+                    dx = 0
+                    dy = 20
 
-        pygame.draw.rect(dis, black, [pomme[0], pomme[1], 20, 20])
-        pomme[0] = random.randint(0, (L-20)/20)*20
-        pomme[1] = random.randint(0, (H-20)/20)*20
+        # on avance
+        queue = copy(l[n-1])
+        for k in range(0, n-1):
+            l[n-1-k] = copy(l[n-2-k])
+        l[0][0] += dx
+        l[0][1] += dy
+        dis.fill(black)
 
-    # une pomme verte peut apparaitre
-    #pomme_coupe(score, pomme_coupe)
-    # si il rencontre une pomme verte sa taille est divisé par 2
-    #coll_pomme_coupe(l, pomme_coupe)
+        # detection mur ou soit même
+        # detection_collision_bordure(l, border, game_over)
+        # detection_auto_collision(l, collision, game_over)
 
-    n = len(l)  # taille du serpent après avoir peut être mangé une pomme
-    pygame.draw.rect(dis, red, [pomme[0], pomme[1], 20, 20])
-    for x in l:
-        pygame.draw.rect(dis, violet, [x[0], x[1], 20, 20])
-    pygame.display.update()
+        # lorsqu'on touche le bord
+        if border and (l[0][0] < 20 or l[0][0] > L-20 or l[0][1] < 20 or l[0][1] > H-20):
+            game_close = True
+        # si bord désactivé on passe de l'autre coté
+        if not border and (l[0][0] < 10 or l[0][0] > L-10 or l[0][1] < 10 or l[0][1] > H-10):
+            l[0][0] = l[0][0] % L
+            l[0][1] = l[0][1] % H
 
-    # on affiche le score
-    score_font = pygame.font.SysFont("Times new roman", 35)
-    value = score_font.render("Your Score: " + str(score), True, red)
-    dis.blit(value, [300, 0])
+        # lorsqu'on se touche
+        for k in range(1, len(l)):
+            if n > 3:
+                if collision and l[0][0] == l[k][0] and l[0][1] == l[k][1]:
+                    game_close = True
 
-    pygame.display.update()
-    clock.tick(15)
+        # lorsqu'on touche la pomme
+        if pomme == l[0]:
+            score += 1
+            l.append([queue[0], queue[1]])
 
-message("Game Over", red, dis)  # Lola
-pygame.display.update()  # Lola
-time.sleep(1)  # Lola
+            pygame.draw.rect(dis, black, [pomme[0], pomme[1], 20, 20])
+            pomme[0] = random.randint(0, (L-20)/20)*20
+            pomme[1] = random.randint(0, (H-20)/20)*20
 
-pygame.quit()
-quit()
+        pygame.draw.rect(dis, red, [pomme[0], pomme[1], 20, 20])
+        # une pomme verte peut apparaitre
+        pomme_coupe2(score, pomme_coupe)
+        # si il rencontre une pomme verte sa taille est divisé par 2
+        l, score = coll_pomme_coupe(l, score, pomme_coupe)
+
+        n = len(l)  # taille du serpent après avoir peut être mangé une pomme
+        if pomme_coupe[2]:
+            pygame.draw.rect(
+                dis, vert, [pomme_coupe[0], pomme_coupe[1], 20, 20])
+
+        for x in l:
+            pygame.draw.rect(dis, violet, [x[0], x[1], 20, 20])
+        pygame.display.update()
+
+        # on affiche le score
+        score_font = pygame.font.SysFont("Times new roman", 35)
+        value = score_font.render("Your Score: " + str(score), True, red)
+        dis.blit(value, [300, 0])
+
+        pygame.display.update()
+        clock.tick(15)
+    pygame.quit()
+    quit()
+
+
+game_loop()
